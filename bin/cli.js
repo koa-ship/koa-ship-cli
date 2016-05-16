69a406b225b44203e1011d57781cb0c1a22c322e
Sean Dou <douguohui@gmail.com>
2016-05-16 17:30:55 +0800

    cli boot

--- bin/cli.js
+++ bin/cli.js
@@ -2,4 +2,13 @@
 
 'use strict';
 
-console.log('Enjoy your sailing:)');
+const App = require('./../lib');
+
+let app = new App(process.cwd());
+if (app.inKoaShipApp()) {
+  // do generate or something else commands.
+  app.doOtherCommands();
+} else {
+  // do new command.
+  app.doNew();
+}
